76db24703f74ee3ac66cfde697a660f03fb3333c
RomaBureacov <RomanBureacov@gmail.com>
2025-02-26 21:27:45 -0800

    revised and appended constants to BaseLayout
    created recursive method to set antialiasing for all components that are part of the frame in BaseLayout

--- TCSS305-Group-Project-main/src/edu/uw/tcss/app/BaseLayout.java
+++ TCSS305-Group-Project-main/src/edu/uw/tcss/app/BaseLayout.java
@@ -1,23 +1,14 @@
 package edu.uw.tcss.app;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Insets;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.BorderFactory;
-import javax.swing.border.Border;
+import java.awt.*;
+import javax.swing.*;
 
 
 /**
  * BorderLayout base Panel.
- * @author Kassie Whitney
+ * @author Roman Bureacov
  * @author Zainab Stanikzy
+ * @author Kassie Whitney
  * @version 2.26.25
  */
 public final class BaseLayout extends JPanel {
@@ -26,20 +17,18 @@ public final class BaseLayout extends JPanel {
 
     private static final int J_FRAME_HEIGHT = 700;
 
-    private static final int GAME_BOARD_WIDTH = 200;
+    // the amount the west panel should take up on the screen
+    private static final double WEST_BOARD_PERCENTAGE = 3.0 / 5;
 
-    private static final int GAME_BOARD_HEIGHT = GAME_BOARD_WIDTH * 2;
+    private static final int WEST_BOARD_WIDTH = (int) (J_FRAME_WIDTH * WEST_BOARD_PERCENTAGE);
 
+    // aspect ratio for the game board should be 2:1
+    private static final int GAME_BOARD_HEIGHT = WEST_BOARD_WIDTH * 2;
 
-    private static final int SIDE_PANEL_WIDTH = 100;
+    private static final int MINOR_PADDING = 5;
+    private static final int MAJOR_PADDING = 10;
 
-    private static final int SIDE_PANEL_HEIGHT = 200;
-    
-
-
-
-
-
+    private static final int EAST_PANEL_WIDTH = (int) (J_FRAME_WIDTH * (1 - WEST_BOARD_PERCENTAGE));
 
     /**
      * Constructor for Base Layout.
@@ -52,57 +41,61 @@ public final class BaseLayout extends JPanel {
     private void layoutComponents() {
         setLayout(new BorderLayout());
 
-        setBorder(BorderFactory.createEmptyBorder(7,7,7,7));
+        setBorder(BorderFactory.createEmptyBorder(MAJOR_PADDING, MAJOR_PADDING, MAJOR_PADDING, MAJOR_PADDING));
 
-        //WEST
-        final JPanel gameBoard = new JPanel();
-        gameBoard.setPreferredSize(new Dimension(GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT));
-        gameBoard.setBackground(Color.RED);
+        // game board lives on the west
+        final JPanel westBoard = new JPanel();
+        westBoard.setPreferredSize(new Dimension(WEST_BOARD_WIDTH, GAME_BOARD_HEIGHT));
+        westBoard.setBackground(Color.RED);
 
-
-
-
-
+        // information, such as the next piece, controls, and score, live on the east
         final JPanel eastPanel = new JPanel();
         eastPanel.setLayout(new BoxLayout(eastPanel, BoxLayout.Y_AXIS));
-        eastPanel.setPreferredSize(new Dimension(100, GAME_BOARD_HEIGHT));
+        eastPanel.setPreferredSize(new Dimension(EAST_PANEL_WIDTH, GAME_BOARD_HEIGHT));
 
-        eastPanel.setBorder(BorderFactory.createEmptyBorder(0,7,0,0));
+        eastPanel.setBorder(BorderFactory.createEmptyBorder(0, MAJOR_PADDING, 0, 0));
 
-        final JPanel nextPiece = new JPanel();
-        nextPiece.setPreferredSize(new Dimension(100, GAME_BOARD_HEIGHT/3));
-        nextPiece.setBackground(Color.BLUE);
-        eastPanel.add(nextPiece);
+        final JPanel nextPiecePanel = new JPanel();
+        nextPiecePanel.setPreferredSize(new Dimension(EAST_PANEL_WIDTH, GAME_BOARD_HEIGHT / 3));
+        nextPiecePanel.setBackground(Color.BLUE);
+        eastPanel.add(nextPiecePanel);
 
-        eastPanel.add(filler(new Dimension(5,5)));
+        nextPiecePanel.setBorder(BorderFactory.createEmptyBorder(0, 0, MINOR_PADDING, 0));
 
-        final JPanel controls = new JPanel();
-        controls.setPreferredSize(new Dimension(100, GAME_BOARD_HEIGHT/3));
-        controls.setBackground(Color.GREEN);
-        eastPanel.add(controls);
-
-
+        final JPanel controlsPanel = new JPanel();
+        controlsPanel.setPreferredSize(new Dimension(EAST_PANEL_WIDTH, GAME_BOARD_HEIGHT / 3));
+        controlsPanel.setBackground(Color.GREEN);
+        eastPanel.add(controlsPanel);
         
-        eastPanel.add(filler(new Dimension(5,5)));
+        controlsPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, MINOR_PADDING, 0));
+
+        final JPanel scorePanel = new JPanel();
+        scorePanel.setPreferredSize(new Dimension(EAST_PANEL_WIDTH, GAME_BOARD_HEIGHT/3));
+        scorePanel.setBackground(Color.GREEN);
+        eastPanel.add(scorePanel);
 
 
-        final JPanel score = new JPanel();
-        score.setPreferredSize(new Dimension(100, GAME_BOARD_HEIGHT/3));
-        score.setBackground(Color.GREEN);
-        eastPanel.add(score);
-
-
-        add(gameBoard, BorderLayout.WEST);
+        add(westBoard, BorderLayout.WEST);
         add(eastPanel, BorderLayout.EAST);
 
-
         this.setOpaque(true);
-
-
     }
-    private static Box.Filler filler(final Dimension theDim) {
-        return new Box.Filler(theDim, theDim, theDim);
+
+    private static void enableAntiAliasing(final Component theCont) {
+        final Graphics2D g = (Graphics2D) (theCont.getGraphics());
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     }
+
+    // recursive function to enable antialiasing on all components and subcomponents
+    private static void setAllAntiAliasing(final Container theComp) {
+        for (final Component comp : theComp.getComponents()) {
+            if (comp instanceof Container) {
+                setAllAntiAliasing((Container) comp);
+            }
+            enableAntiAliasing(comp);
+        }
+    }
+
     /**
      * Creates the JFrame.
      */
@@ -111,17 +104,20 @@ public final class BaseLayout extends JPanel {
 
         final JFrame window = new JFrame("Group 3 Tetris");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
 
         final FileMenu menuBar = new FileMenu(window);
-        
 
         window.setJMenuBar(menuBar);
 
-        window.setSize(J_FRAME_WIDTH, J_FRAME_HEIGHT);
+        // window.setSize(J_FRAME_WIDTH, J_FRAME_HEIGHT);
 
         window.setContentPane(mainPanel);
 
         window.pack();
+
+        setAllAntiAliasing(window.getContentPane());
+
         window.setVisible(true);
     }
 }
