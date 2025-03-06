bd9aa109c3702b7c9787bbbe4e226b47b6ce42ff
RomaBureacov <RomanBureacov@gmail.com>
2025-03-05 18:15:57 -0800

    created a wrapper class that handles all the keymappings

--- TCSS305-Group-Project-main/src/edu/uw/tcss/app/BaseLayout.java
+++ TCSS305-Group-Project-main/src/edu/uw/tcss/app/BaseLayout.java
@@ -1,11 +1,16 @@
 package edu.uw.tcss.app;
 
+
 import edu.uw.tcss.model.TetrisGame;
+import edu.uw.tcss.util.KeyMapper;
+
+import static edu.uw.tcss.util.KeyMapper.TetrominoControls;
+import static edu.uw.tcss.util.KeyMapper.GameControls;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
+import java.security.Key;
 import javax.swing.*;
 
 
@@ -34,7 +39,8 @@ public final class BaseLayout extends JPanel {
     private static final int EAST_PANEL_WIDTH = J_FRAME_WIDTH - WEST_PANEL_WIDTH;
     private static final int EAST_PANEL_COMP_HEIGHT = GAME_BOARD_HEIGHT / 3;
 
-    private static final TetrisGame game = new TetrisGame();
+    private final TetrisGame myTetrisGame = new TetrisGame();
+    private final KeyMapper myKeyMapper;
 
     /**
      * Constructor for Base Layout.
@@ -42,6 +48,10 @@ public final class BaseLayout extends JPanel {
     public BaseLayout() {
         super();
         layoutComponents();
+
+        myKeyMapper = new KeyMapper(this, myTetrisGame);
+
+        setupKeys();
     }
 
     private void layoutComponents() {
@@ -85,12 +95,13 @@ public final class BaseLayout extends JPanel {
         add(westPanel, BorderLayout.WEST);
         add(eastPanel, BorderLayout.EAST);
 
-        final String leftKey = "left";
-        final InputMap imap = westPanel.getInputMap();
-        imap.put(KeyStroke.getKeyStroke('a'), 'l');
-        final ActionMap amap = westPanel.getActionMap();
-        amap.put('l', new GameControlsAction(GameControlsAction.Controls.LEFT));
+    }
 
+    private void setupKeys() {
+        myKeyMapper.mapTetrominoAction(KeyStroke.getKeyStroke("a"), TetrominoControls.LEFT);
+        myKeyMapper.mapTetrominoAction(KeyStroke.getKeyStroke("s"), TetrominoControls.DOWN);
+        myKeyMapper.mapTetrominoAction(KeyStroke.getKeyStroke("d"), TetrominoControls.RIGHT);
+        myKeyMapper.mapTetrominoAction(KeyStroke.getKeyStroke("space"), TetrominoControls.DROP);
     }
 
     /**
@@ -114,61 +125,4 @@ public final class BaseLayout extends JPanel {
         window.setVisible(true);
     }
 
-    // key listeners, created using abstract action
-    private static class GameControlsAction extends AbstractAction {
-        enum Controls {
-            LEFT,
-            RIGHT,
-            DOWN,
-            DROP,
-            ROTATE_CW,
-            ROTATE_CCW,
-        }
-
-        GameControlsAction(final Controls theControlBind) {
-            putValue(Action.NAME, "Control Bind");
-            putValue("bind", theControlBind);
-        }
-
-        @Override
-        public void actionPerformed(final ActionEvent theEvent) {
-            final Controls control = (Controls) getValue("bind");
-            switch (control) {
-                case Controls.LEFT -> game.left();
-                case Controls.RIGHT -> game.right();
-                case Controls.DOWN -> game.down();
-                case Controls.DROP -> game.drop();
-                case Controls.ROTATE_CW -> game.rotateCW();
-                case Controls.ROTATE_CCW -> game.rotateCCW();
-                default -> throw new EnumConstantNotPresentException(Controls.class, control.name());
-            }
-            System.out.println("Action: " + control);
-        }
-    }
-
-    private static class GameActions extends AbstractAction {
-        enum Controls {
-            END_GAME,
-            NEW_GAME,
-            PAUSE,
-            UNPAUSE,
-            TOGGLE_PAUSE,
-        }
-
-        GameActions(final int theControlBind) {
-            putValue(Action.NAME, "Control Bind");
-            putValue("bind", theControlBind);
-        }
-
-        @Override
-        public void actionPerformed(final ActionEvent theEvent) {
-            switch (getValue("bind")) {
-                case Controls.END_GAME -> game.endGame();
-                case Controls.NEW_GAME -> game.newGame();
-                case Controls.PAUSE -> game.pause();
-                case Controls.UNPAUSE -> game.unPause();
-                default -> game.togglePause();
-            }
-        }
-    }
 }
