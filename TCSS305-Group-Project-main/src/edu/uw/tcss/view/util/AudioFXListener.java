c7b6fb848a28b98b217e9e09a8e525ed43ffc5e6
RomaBureacov <RomanBureacov@gmail.com>
2025-03-14 01:55:14 -0700

    cleaned up AudioFXListener a little

--- TCSS305-Group-Project-main/src/edu/uw/tcss/view/util/AudioFXListener.java
+++ TCSS305-Group-Project-main/src/edu/uw/tcss/view/util/AudioFXListener.java
@@ -3,7 +3,6 @@ package edu.uw.tcss.view.util;
 import static edu.uw.tcss.model.GameControls.GameState;
 import static edu.uw.tcss.model.PropertyChangeEnabledGameControls.PROPERTY_FROZEN_BLOCKS;
 import static edu.uw.tcss.model.PropertyChangeEnabledGameControls.PROPERTY_GAME_STATE;
-import static edu.uw.tcss.model.PropertyChangeEnabledGameControls.PROPERTY_ROWS_CLEARED;
 import static edu.uw.tcss.view.util.AudioFXManager.Channels;
 
 import java.beans.PropertyChangeEvent;
@@ -30,15 +29,15 @@ public class AudioFXListener implements PropertyChangeListener {
         switch (theEvent.getPropertyName()) {
             case PROPERTY_GAME_STATE -> handleGameState((GameState) theEvent.getNewValue());
             case PROPERTY_FROZEN_BLOCKS ->
-                AudioFXManager.playSoundFX(AudioFXManager.Channels.LANDING_FX);
+                AudioFXManager.playSoundFX(Channels.LANDING_FX);
             default -> { }
         }
     }
 
     private void handleGameState(final GameState theGameState) {
         switch (theGameState) {
-            case GameState.OVER -> AudioFXManager.playSoundFX(AudioFXManager.Channels.GAME_OVER);
-            case GameState.PAUSED -> AudioFXManager.playSoundFX(AudioFXManager.Channels.PAUSE_FX);
+            case GameState.OVER -> AudioFXManager.playSoundFX(Channels.GAME_OVER);
+            case GameState.PAUSED -> AudioFXManager.playSoundFX(Channels.PAUSE_FX);
             default -> { }
         }
     }
