49c347f14d1b1f141cddb2d6940e1d558723c7b2
JamesStrand <jamess37@uw.edu>
2025-03-14 22:21:42 -0700

    finally got rid of cyclomatic complexity in gameboard

--- TCSS305-Group-Project-main/src/edu/uw/tcss/view/app/GameBoardPanel.java
+++ TCSS305-Group-Project-main/src/edu/uw/tcss/view/app/GameBoardPanel.java
@@ -303,8 +303,6 @@ public class GameBoardPanel extends JPanel implements PropertyChangeListener {
 
     private void gameStateSwitches(final PropertyChangeEvent theEvent) {
 
-        int test = 0;
-
         switch (theEvent.getNewValue()) {
             case GameState.NEW -> {
                 myGamePaused = false;
